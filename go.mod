bf0a5f331d3f08a02858ddff8adde1f33e33c7a1
Genesis Font <genesis.font@opskins.com>
2023-09-26 20:17:06 -0400

    struct for account, new methods to initialize

--- go.mod
+++ go.mod
@@ -8,6 +8,14 @@ require (
 )
 
 require (
+	github.com/go-sql-driver/mysql v1.7.0 // indirect
+	github.com/jinzhu/inflection v1.0.0 // indirect
+	github.com/jinzhu/now v1.1.5 // indirect
+	gorm.io/gorm v1.25.1 // indirect
+)
+
+require (
 	cloud.google.com/go v0.99.0 // indirect
 	github.com/shopspring/decimal v1.2.0 // indirect
+	gorm.io/driver/mysql v1.5.1
 )
--- main.go
+++ main.go
@@ -4,39 +4,128 @@ import (
 	"fmt"
 	"log"
 	"os"
+	"time"
 
 	"github.com/alpacahq/alpaca-trade-api-go/v2/alpaca"
 	"github.com/joho/godotenv"
-	// "github.com/shopspring/decimal"	// Used for decimal type
+	"github.com/shopspring/decimal"
+	"gorm.io/driver/mysql"
+	"gorm.io/gorm"
+	"gorm.io/gorm/clause"
 )
 
-func getAccount(client alpaca.Client) {
+type Account struct {
+	ID                          uint `gorm:"primaryKey"`
+	AccountID                   string
+	account_blocked             bool
+	account_number              string
+	crypto_status               string
+	currency                    string
+	cash                        decimal.Decimal
+	portfolio_value             decimal.Decimal
+	non_marginable_buying_power string
+	accrued_fees                string
+	pending_transfer_in         string
+	pending_transfer_out        string
+	pattern_day_trader          bool
+	trade_suspended_by_user     bool
+	trading_blocked             bool
+	transfers_blocked           bool
+	short_market_value          decimal.Decimal
+	equity                      decimal.Decimal
+	last_equity                 decimal.Decimal
+	multiplier                  string
+	buying_power                decimal.Decimal
+	shorting_enabled            bool
+	long_market_value           decimal.Decimal
+	initial_margin              decimal.Decimal
+	maintenance_margin          decimal.Decimal
+	sma                         string
+	daytrade_count              int64
+	last_maintenance_margin     decimal.Decimal
+	daytrading_buying_power     decimal.Decimal
+	regt_buying_power           decimal.Decimal
+	created_at                  time.Time
+	CreatedAt                   time.Time
+	UpdatedAt                   time.Time
+	DeletedAt                   gorm.DeletedAt `gorm:"index"`
+}
+
+func getAccount(client alpaca.Client, db gorm.DB) {
 	// Get account information
 	acct, err := client.GetAccount()
+
+	var account Account = Account{
+		AccountID:       *&acct.ID,
+		account_blocked: *&acct.AccountBlocked,
+		account_number:  *&acct.AccountNumber,
+		portfolio_value: *&acct.PortfolioValue,
+		crypto_status:   *&acct.Status,
+		currency:        *&acct.Currency,
+		cash:            *&acct.Cash,
+		// non_marginable_buying_power: *&acct.
+		// accrued_fees: *&acct.
+		// pending_transfer_in: *&acct.
+		pattern_day_trader: *&acct.PatternDayTrader,
+		// trade_suspended_by_user: *&acct.
+		trading_blocked:    *&acct.TradingBlocked,
+		transfers_blocked:  *&acct.TransfersBlocked,
+		short_market_value: *&acct.ShortMarketValue,
+		equity:             *&acct.Equity,
+		last_equity:        *&acct.LastEquity,
+		multiplier:         *&acct.Multiplier,
+		buying_power:       *&acct.BuyingPower,
+		shorting_enabled:   *&acct.ShortingEnabled,
+		long_market_value:  *&acct.LongMarketValue,
+		initial_margin:     *&acct.InitialMargin,
+		maintenance_margin: *&acct.MaintenanceMargin,
+		// sma: *&acct.
+		daytrade_count:          *&acct.DaytradeCount,
+		last_maintenance_margin: *&acct.LastMaintenanceMargin,
+		daytrading_buying_power: *&acct.DaytradingBuyingPower,
+		regt_buying_power:       *&acct.RegTBuyingPower,
+		created_at:              *&acct.CreatedAt,
+		CreatedAt:               time.Now(),
+		UpdatedAt:               time.Now(),
+	}
+
 	if err != nil {
 		// Print error
 		fmt.Printf("Error getting account information: %v", err)
 	} else {
 		// Print account information
+		fmt.Printf("Account ID: %+v\n", *&acct.ID)
 		fmt.Printf("Account: %+v\n", *acct)
+
+		db.Clauses(clause.OnConflict{
+			UpdateAll: true,
+		}).Create(&account)
 	}
 }
 
 func main() {
 
-	loadEnvironment()
+	db := loadEnvironment()
 
 	client := prepAlpaca()
 
-	getAccount(client)
+	getAccount(client, *db)
 }
 
-func loadEnvironment() {
+func loadEnvironment() *gorm.DB {
 	err := godotenv.Load()
 
 	if err != nil {
 		log.Fatal("Error loading .env file")
 	}
+
+	dsn := "user:pass@tcp(127.0.0.1:3306)/dbname?charset=utf8mb4&parseTime=True&loc=Local"
+	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
+
+	// Migrate the schema
+	db.AutoMigrate(&Account{})
+
+	return db
 }
 
 func prepAlpaca() alpaca.Client {
