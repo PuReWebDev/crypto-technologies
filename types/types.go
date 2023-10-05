e523db8ee60ab49418f9fc2aa3d31f82922f7a43
Genesis Font <genesis.font@opskins.com>
2023-10-05 15:10:02 -0400

    added more type definitions

--- types/types.go
+++ types/types.go
@@ -68,3 +68,65 @@ type Order struct {
 	LimitPrice     *decimal.Decimal
 	Status         string
 }
+
+type ListOrdersRequest struct {
+	Status    *string    `json:"status"`
+	After     *time.Time `json:"after"`
+	Until     *time.Time `json:"until"`
+	Limit     *int       `json:"limit"`
+	Direction *string    `json:"direction"`
+	Nested    *bool      `json:"nested"`
+	Symbols   *string    `json:"symbols"`
+	Side      *string    `json:"side"`
+}
+
+type Side string
+type TimeInForce string
+type OrderClass string
+type OrderType string
+
+type TakeProfit struct {
+	LimitPrice *decimal.Decimal `json:"limit_price"`
+}
+
+type StopLoss struct {
+	LimitPrice *decimal.Decimal `json:"limit_price"`
+	StopPrice  *decimal.Decimal `json:"stop_price"`
+}
+
+type PlaceOrderRequest struct {
+	AccountID     string           `json:"-"`
+	AssetKey      *string          `json:"symbol"`
+	Qty           *decimal.Decimal `json:"qty"`
+	Notional      *decimal.Decimal `json:"notional"`
+	Side          Side             `json:"side"`
+	Type          OrderType        `json:"type"`
+	TimeInForce   TimeInForce      `json:"time_in_force"`
+	LimitPrice    *decimal.Decimal `json:"limit_price"`
+	ExtendedHours bool             `json:"extended_hours"`
+	StopPrice     *decimal.Decimal `json:"stop_price"`
+	ClientOrderID string           `json:"client_order_id"`
+	OrderClass    OrderClass       `json:"order_class"`
+	TakeProfit    *TakeProfit      `json:"take_profit"`
+	StopLoss      *StopLoss        `json:"stop_loss"`
+	TrailPrice    *decimal.Decimal `json:"trail_price"`
+	TrailPercent  *decimal.Decimal `json:"trail_percent"`
+}
+
+type Position struct {
+	AssetID        string           `json:"asset_id"`
+	Symbol         string           `json:"symbol"`
+	Exchange       string           `json:"exchange"`
+	Class          string           `json:"asset_class"`
+	AccountID      string           `json:"account_id"`
+	EntryPrice     decimal.Decimal  `json:"avg_entry_price"`
+	Qty            decimal.Decimal  `json:"qty"`
+	Side           string           `json:"side"`
+	MarketValue    *decimal.Decimal `json:"market_value"`
+	CostBasis      decimal.Decimal  `json:"cost_basis"`
+	UnrealizedPL   *decimal.Decimal `json:"unrealized_pl"`
+	UnrealizedPLPC *decimal.Decimal `json:"unrealized_plpc"`
+	CurrentPrice   *decimal.Decimal `json:"current_price"`
+	LastdayPrice   *decimal.Decimal `json:"lastday_price"`
+	ChangeToday    *decimal.Decimal `json:"change_today"`
+}
