4804df3745daf00ba8eb76a23d3730f5f97215a8
Genesis Font <genesis.font@opskins.com>
2023-10-08 12:59:01 -0400

    new methods to listen for market and trade updates

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
 package main
 
 import (
+	"context"
 	"crypto-technologies/types"
 	"fmt"
 	"log"
 	"os"
+	"os/signal"
+	"sync/atomic"
 	"time"
 
 	"github.com/alpacahq/alpaca-trade-api-go/v3/alpaca"
+	"github.com/alpacahq/alpaca-trade-api-go/v3/marketdata"
+	"github.com/alpacahq/alpaca-trade-api-go/v3/marketdata/stream"
 	"github.com/joho/godotenv"
 	"github.com/shopspring/decimal"
 	"gorm.io/driver/mysql"
@@ -49,6 +54,116 @@ func main() {
 	placeOrder(client, *db)
 	listPositions(client, *db)
 	getAsset(client)
+	listenForMarket()
+	listenForTrades()
+}
+
+func listenForMarket() {
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// Creating a client that connects to iex
+	c := stream.NewCryptoClient(marketdata.US,
+		stream.WithLogger(stream.DefaultLogger()),
+		// configuring initial subscriptions and handlers
+		stream.WithCryptoTrades(func(ct stream.CryptoTrade) {
+			fmt.Printf("TRADE: %+v\n", ct)
+		}, "*"),
+		stream.WithCryptoQuotes(func(cq stream.CryptoQuote) {
+			fmt.Printf("QUOTE: %+v\n", cq)
+		}, "BTC/USD"),
+		stream.WithCryptoOrderbooks(func(cob stream.CryptoOrderbook) {
+			fmt.Printf("ORDERBOOK: %+v\n", cob)
+		}, "BTC/USD"),
+	)
+	if err := c.Connect(ctx); err != nil {
+		panic(err)
+	}
+	if err := <-c.Terminated(); err != nil {
+		panic(err)
+	}
+}
+
+func listenForTrades() {
+	var tradeCount, quoteCount, barCount int32
+	// modify these according to your needs
+	tradeHandler := func(t stream.Trade) {
+		atomic.AddInt32(&tradeCount, 1)
+	}
+	quoteHandler := func(q stream.Quote) {
+		atomic.AddInt32(&quoteCount, 1)
+	}
+	barHandler := func(b stream.Bar) {
+		atomic.AddInt32(&barCount, 1)
+	}
+
+	ctx, cancel := context.WithCancel(context.Background())
+	defer cancel()
+
+	// setting up cancelling upon interrupt
+	s := make(chan os.Signal, 1)
+	signal.Notify(s, os.Interrupt)
+	go func() {
+		<-s
+		cancel()
+	}()
+
+	// Creating a client that connexts to iex
+	c := stream.NewStocksClient(
+		marketdata.IEX,
+		// configuring initial subscriptions and handlers
+		stream.WithTrades(tradeHandler, "SPY"),
+		stream.WithQuotes(quoteHandler, "AAPL", "SPY"),
+		stream.WithBars(barHandler, "AAPL", "SPY"),
+		// use stream.WithDailyBars to subscribe to daily bars too
+		// use stream.WithCredentials to manually override envvars
+		// use stream.WithHost to manually override envvar
+		// use stream.WithLogger to use your own logger (i.e. zap, logrus) instead of log
+		// use stream.WithProcessors to use multiple processing gourotines
+		// use stream.WithBufferSize to change buffer size
+		// use stream.WithReconnectSettings to change reconnect settings
+	)
+
+	// periodically displaying number of trades/quotes/bars received so far
+	go func() {
+		for {
+			time.Sleep(1 * time.Second)
+			fmt.Println("trades:", tradeCount, "quotes:", quoteCount, "bars:", barCount)
+		}
+	}()
+
+	if err := c.Connect(ctx); err != nil {
+		log.Fatalf("could not establish connection, error: %s", err)
+	}
+	fmt.Println("established connection")
+
+	// starting a goroutine that checks whether the client has terminated
+	go func() {
+		err := <-c.Terminated()
+		if err != nil {
+			log.Fatalf("terminated with error: %s", err)
+		}
+		fmt.Println("exiting")
+		os.Exit(0)
+	}()
+
+	time.Sleep(3 * time.Second)
+	// Adding AAPL trade subscription
+	if err := c.SubscribeToTrades(tradeHandler, "AAPL"); err != nil {
+		log.Fatalf("error during subscribing: %s", err)
+	}
+	fmt.Println("subscribed to AAPL trades")
+
+	time.Sleep(3 * time.Second)
+	// Unsubscribing from AAPL quotes
+	if err := c.UnsubscribeFromQuotes("AAPL"); err != nil {
+		log.Fatalf("error during unsubscribing: %s", err)
+	}
+	fmt.Println("unsubscribed from AAPL quotes")
+
+	// and so on...
+	time.Sleep(100 * time.Second)
+	fmt.Println("we're done")
 }
 
 func loadEnvironment() *gorm.DB {
@@ -66,7 +181,7 @@ func loadEnvironment() *gorm.DB {
 	}
 
 	// Migrate the schema
-	db.AutoMigrate(&types.Account{}, &types.Order{})
+	db.AutoMigrate(&types.Account{}, &types.Order{}, &types.CryptoQuote{})
 
 	return db
 }
